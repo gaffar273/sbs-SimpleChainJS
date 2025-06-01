ffe4a6df7da02910648c8f936ea026374554badc
ABDUL GAFFAR <gaffarsk273@gmail.com>
2025-06-01 21:25:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const { Block, BlockChain } = require('./blockChain')
 
-let agscoin = new BlockChain()
+let agsChain = new BlockChain()
 
 console.log('Mining block 1...')
-agscoin.addBlock(new Block(2, '03/02/25', { amount: 4 }))
+agsChain.addBlock(new Block(2, '03/02/25', { amount: 4 }))
 
 console.log('Mining block 2...')
-agscoin.addBlock(new Block(3, '01/12/25', { amount: 5 }))
+agsChain.addBlock(new Block(3, '01/12/25', { amount: 5 }))
 
-console.log("Blockchain valid?", agscoin.isChainValid())
+console.log("Is Blockchain valid?", agsChain.isChainValid())
